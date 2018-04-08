02502f5bcad15101e7787ae9d6f9419a4739f10c
uryyyyyyy <koki305@gmail.com>
2018-04-08 21:19:24 +0900

    6.0-RC3

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,14 +2,20 @@ const webpack = require('webpack');
 const path = require("path");
 
 module.exports = {
+  mode: 'development',
   entry: {
-    'polyfills': './src/polyfills.ts',
-    'app': './src/main.ts'
+    polyfills: './src/polyfills.ts',
+    app: './src/main.ts'
   },
   output: {
     path: path.resolve('dist'),
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js'
+    filename: '[name].js'
+  },
+  optimization: {
+    splitChunks: {
+      name: 'common',
+      chunks: 'initial'
+    }
   },
   devtool: 'source-map',
 
@@ -22,26 +28,23 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
-        use: {
-          loader: '@ngtools/webpack',
-          options: {
-            mainPath: "src/main.ts",
-            tsConfigPath: 'tsconfig.json',
-            sourceMap: true
-          }
-        }
+        test: /\.html$/,
+        loader: 'raw-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'raw-loader'
+      },
+      {
+        test: /\.ts$/,
+        loader: ['awesome-typescript-loader', 'angular2-template-loader']
       }
     ]
   },
 
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'polyfills']
-    }),
-
     new webpack.ContextReplacementPlugin(
-      /\@angular(\\|\/)core(\\|\/)esm5/,
+      /\@angular(\\|\/)core(\\|\/)fesm5/,
       path.resolve('src'),
       {}
     ),
