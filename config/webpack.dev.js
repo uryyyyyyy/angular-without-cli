391265caf45a4e4b383f47a2ba26bcc5261ba99f
uryyyyyyy <koki305@gmail.com>
2018-04-09 18:48:24 +0900

    6.0-RC3

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -28,16 +28,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.html$/,
-        loader: 'raw-loader'
-      },
-      {
-        test: /\.css$/,
-        loader: 'raw-loader'
-      },
-      {
         test: /\.ts$/,
-        loader: ['awesome-typescript-loader', 'angular2-template-loader']
+        loader: ['ts-loader', 'angular2-template-loader']
       }
     ]
   },
